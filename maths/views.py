8da212dcde52d42dcd32a9f01e00e3f854e5f4a6
Piotr Gaszczynski <piotrek.gaszczynski@gmail.com>
2024-05-07 19:25:53 +0200

    Change maths

--- maths/views.py
+++ maths/views.py
@@ -4,42 +4,65 @@ from django.http import HttpResponse
 def math(request):
    return HttpResponse("Tu będzie matma")
 
+
 def add(request, a, b):
 	a, b = int(a), int(b)
-	c = {"a":str(a), "b":str(b), "operacja": "+" ,"wynik":str(a+b)}
+	c = {
+    	"a":str(a), 
+      	"b":str(b), 
+      	"operacja": "+",
+      	"wynik":str(a+b), 
+      	"title": "dodawanie"
+       }
 	return render(
     	    request=request,
-    	    template_name="maths/main.html",
+    	    template_name="maths/operation.html",
     	    context=c
 	)
+
 
 def sub(request, a, b):
 	a, b = int(a), int(b)
-	c = {"a":str(a), "b":str(b), "operacja": "-" ,"wynik":str(a-b)}
+	c = {
+     	"a":str(a),
+      	"b":str(b),
+       	"operacja": "-",
+        "wynik":str(a-b),
+        "title": "odejmowanie"
+        }
 	return render(
     	    request=request,
-    	    template_name="maths/main.html",
+    	    template_name="maths/operation.html",
     	    context=c
 	)
+
 
 def mul(request, a, b):
 	a, b = int(a), int(b)
-	c = {"a":str(a), "b":str(b), "operacja": "*" ,"wynik":str(a*b)}
+	c = {"a":str(a),
+      	"b":str(b),
+      	"operacja": "*",
+      	"wynik":str(a*b),
+      	"title": "mnozenie"
+     	}
 	return render(
     	    request=request,
-    	    template_name="maths/main.html",
+    	    template_name="maths/operation.html",
     	    context=c
 	)
 
+
 def div(request, a, b):
-   a, b = int(a), int(b)
-   
-   if b == 0:
-      return HttpResponse("Nie dziel przez 0")
-   
-   c = {"a":str(a), "b":str(b), "operacja": "/" ,"wynik":str(a/b)}
-   return render(
-    	request=request,
-      template_name="maths/main.html",
-    	context=c
+    a, b = int(a), int(b)
+    c = {
+        "a":str(a),
+        "b":str(b),
+        "operacja": "/",
+        "wynik":str(a/b) if b != 0 else None,
+        "title": "dzielenie",
+        }
+    return render(
+		request=request,
+		template_name="maths/operation.html",
+		context=c
 	)
