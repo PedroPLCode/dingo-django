1d8c71a8d472b33dfb4d5ba440009d97ffcfa744
Piotr Gaszczynski <piotrek.gaszczynski@gmail.com>
2024-05-07 18:07:33 +0200

    Add greetings

--- greetings/urls.py
+++ greetings/urls.py
@@ -1,10 +1,7 @@
 from django.urls import path
-from .views import math, add, sub, mul, div
+from .views import show_greetings
 
 urlpatterns = [
-   path('/', math),
-   path('add/<int:a>/<b>', add),
-   path('sub/<int:a>/<b>', sub),
-   path('mul/<int:a>/<b>', mul),
-   path('div/<int:a>/<b>', div),
+   path('', show_greetings),
+   path('<name>', show_greetings)
 ]
--- maths/urls.py
+++ maths/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import math, add, sub, mul, div
 
 urlpatterns = [
-   path('/', math),
+   path('', math),
    path('add/<int:a>/<b>', add),
    path('sub/<int:a>/<b>', sub),
    path('mul/<int:a>/<b>', mul),
