2d8e0fc76701324a513859ec5673c668995be238
Piotr Gaszczynski <piotrek.gaszczynski@gmail.com>
2024-05-08 23:03:12 +0200

    Add app posts and posts.models

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=30, unique=True, null=False)
+    content = models.TextField(null=False)
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+    author = models.ForeignKey(
+        'posts.Author',
+        on_delete=models.CASCADE,
+        null=False,
+        blank=False
+    )
+    
+    def __str__(self):
+        return f"id:{self.id}, title={self.title}, content={self.content}, created={self.created}, modified={self.modified}"
+
+
+class Author(models.Model):
+    nick = models.CharField(max_length=30, null=False, unique=True)
+    email = models.EmailField(null=False, unique=True)
+    
+    def __str__(self):
+        return f"id:{self.id}, nick={self.nick}, email={self.email}"
